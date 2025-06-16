04c59a0073fd2ef09e720c709a41c9786d7ed1ab
d-kimsuon <dev.kimuson@gmail.com>
2025-06-16 11:40:23 +0900

    add dmm api schema

---
+++ orval.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  dmm: {
+    input: {
+      target: "./schema/out/@typespec/openapi3/openapi.yaml",
+    },
+    output: {
+      mode: "single",
+      client: "fetch",
+      httpClient: "fetch",
+      baseUrl: "https://api.dmm.com/affiliate/v3",
+      target: "src/lib/api",
+    },
+  },
+});
